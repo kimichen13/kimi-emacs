4c7ee4e8d7f9b3ba3b9d7f5be2e1b8b8d0c1d7a0
Kimi Chen <kimichen13@gmail.com>
2019-04-23 00:11:41 -0400

    Add projectile.

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;; kimi chen's .emacs file
 ;; Created on 14 April 2019 in Miami.
 
+;;; Code:
 (setq delete-old-versions -1 )		; delete excess backup versions silently
 (setq version-control t )		; use version control
 (setq vc-make-backup-files t )		; make backups file even when in version controlled dir
@@ -24,7 +25,7 @@
                          ("gnu"       . "http://elpa.gnu.org/packages/")
                          ("melpa"     . "https://melpa.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
-(package-initialize) 
+(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package) ; unless it is already installed
@@ -38,8 +39,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (dashboard linum-relative powerline smex auto-complete magit avy general use-package))))
+	 (quote
+		(tide flycheck web-mode dashboard linum-relative powerline smex auto-complete magit avy general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -56,6 +57,15 @@
 (use-package avy :ensure t)
 (use-package counsel :ensure t)
 (use-package swiper :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config
+	(define-key projectile-mode-map (kbd "M-m p") 'projectile-command-map)
+	(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+(setq projectile-project-search-path '("~/Documents/Program/"))
+
 
 ;;;; theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
@@ -91,18 +101,16 @@
 ;; To disable shortcut "jump" indicators for each section, set
 ;; (setq dashboard-show-shortcuts nil)
 
-;; (setq dashboard-items '((recents  . 5)
-;;                         (bookmarks . 5)
-;;                         (projects . 5)
-;;                         (agenda . 5)
-;;                         (registers . 5)))
+(setq dashboard-items '((recents  . 5)
+                        ;; (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+			(registers . 5)))
 
 ;;;; AutoComplete
 (use-package auto-complete :ensure t)
 (ac-config-default) ;;; basic configuration
 
-;;;; Company
-					; (add-hook 'after-init-hook 'global-company-mode)
 
 ;;;; smex
 (use-package smex :ensure t)
@@ -115,7 +123,99 @@
 (use-package magit :ensure t)
 (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)  ; full screen
 
+;;;; web-mode
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+
+  (setq web-mode-content-types-alist
+        '(("jsx" . ".*\\.js\\'"))
+        )
+  )
+
+;;;; flycheck
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode t)
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  )
+
+;; ;;;; tern
+;; (use-package tern
+;;   :ensure t
+;;   :config
+;;   (add-hook 'web-mode-hook (lambda () (tern-mode t)))
+;;   :bind (:map tern-mode-keymap
+;;               ("M-*" . tern-pop-find-definition))
+;;   )
+
+;; (use-package company-tern
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'company-backends 'company-tern)
+;;   )
+
+;;;; tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(use-package tide
+  :ensure t
+  :bind (("M-." . tide-jump-to-definition)
+         ("M-," . tide-jump-back)
+         )
+
+  :config
+  (setup-tide-mode)
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+
+  ;; configure javascript-tide checker to run after your default javascript checker
+  (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "js" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; configure jsx-tide checker to run after your default jsx checker
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+  )
+
+(add-hook 'web-mode-hook
+            (lambda ()
+              (set (make-local-variable 'company-backends)
+                   '(
+                      company-tide
+                      ;company-react
+                      company-dabbrev-code
+                      company-keywords
+                      company-files
+                      company-yasnippet))))
+
+
 ;;;; Bind keys
+(setq-default tab-width 2)
+
 (global-set-key (kbd "M-m") nil)  ; Unbind `M-m` to use it as prefix
 
 (general-define-key
@@ -125,6 +225,9 @@
  "l"  '(:ignore t :which-key "linum")
  "lt" 'linum-relative-toggle
 
+;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
  
  "b"  'ivy-switch-buffer  ; change buffer, chose using ivy
  "/"  'counsel-git-grep   ; find string in git project
@@ -132,6 +235,5 @@
  "f"  '(:ignore t :which-key "files")
  "ff" 'counsel-find-file
  "fr" 'counsel-recentf
- "p"  '(:ignore t :which-key "project")
- "pf" '(counsel-git :which-key "find file in git dir")
+
  )
