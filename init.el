62f54c86ac8c3f9c12892670deebb36e758e0253
Kimi Chen <kimichen13@gmail.com>
2021-05-15 16:18:14 +0800

    Fix `Fontset `tty' does not exist` for emacs -nw.

--- init.el
+++ init.el
@@ -6,30 +6,33 @@
 
 ;;; Code:
 
-(when (eq system-type 'darwin)
 
-  ;; default Latin font (e.g. Consolas)
-  ;; (set-default-font "Manoaco")
+(if (display-graphic-p)
+    (when (eq system-type 'darwin)
 
-  (set-face-attribute 'default nil :family "Monaco")
+      ;; default Latin font (e.g. Consolas)
+      ;; (set-default-font "Manoaco")
 
-  ;; default font size (point * 10)
-  ;;
-  ;; WARNING!  Depending on the default font,
-  ;; if the size is not supported very well, the frame will be clipped
-  ;; so that the beginning of the buffer may not be visible correctly.
-  (set-face-attribute 'default nil :height 145)
+      (set-face-attribute 'default nil :family "Monaco")
 
-  ;; use specific font for Korean charset.
-  ;; if you want to use different font size for specific charset,
-  ;; add :size POINT-SIZE in the font-spec.
-  (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
+      ;; default font size (point * 10)
+      ;;
+      ;; WARNING!  Depending on the default font,
+      ;; if the size is not supported very well, the frame will be clipped
+      ;; so that the beginning of the buffer may not be visible correctly.
+      (set-face-attribute 'default nil :height 145)
+
+      ;; use specific font for Korean charset.
+      ;; if you want to use different font size for specific charset,
+      ;; add :size POINT-SIZE in the font-spec.
+      (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
   
-  ;; you may want to add different for other charset in this way.
-  (dolist (charset '(kana han symbol cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font) charset
-                      (font-spec :family "STFangsong" :size 18)
-                      )))
+      ;; you may want to add different for other charset in this way.
+      (dolist (charset '(kana han symbol cjk-misc bopomofo))
+        (set-fontset-font (frame-parameter nil 'font) charset
+                          (font-spec :family "STFangsong" :size 18)
+                          )))
+  )
 
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
