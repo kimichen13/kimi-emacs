309bbf5036bdc259fd210c5f1fb181508dd8c485
Kimi Chen <kimichen13@gmail.com>
2021-05-26 00:37:44 +0800

    Remove unused setting.

--- init.el
+++ init.el
@@ -97,8 +97,6 @@
 			 ))
 (package-initialize)
 
-;; (package-refresh-contents)
-
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package) ; unless it is already installed
   (package-refresh-contents) ; updage packages archive
@@ -111,22 +109,6 @@
   (exec-path-from-shell-initialize))
 
 (require 'use-package)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (htmlize company golden-ratio yaml-mode tide flycheck web-mode dashboard linum-relative powerline smex auto-complete magit avy general use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-block ((t (:background "#969696"))))
- '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF"))))
- '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))))
 
 (use-package yasnippet
   :ensure t
